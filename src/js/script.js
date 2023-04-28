585bf6a071b60b8ee0c8044fb828cc39beed6d57
AaronMcGuirk007 <am12mcgu@siena.edu>
2023-04-28 16:28:03 -0400

    clean up

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const loaderContainer = document.querySelector('.loader-container');
+
+window.addEventListener('load', () => {
+    window.addEventListener('load', () => {
+        loaderContainer.style.display = 'none';
+    });
+});
